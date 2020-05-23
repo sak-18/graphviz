5398793176c9f0127c06e592b0b90885c81e4f16
saketh <saketh181199@gmail.com>
2020-05-24 00:06:50 +0530

    grid generation, also added Dijkstra

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+//import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+import Pathviz from './PathfindingVisualizer/pathviz';
+
+function App() {
+  return (
+    <div className="App">
+      <Pathviz></Pathviz>
+    </div>
+  );
+}
+
+export default App;
